53c7f187f53ae5f5ed03ac8aa5aa89f9231290f0
Adnan Ivarsson <139991430+adnanivar@users.noreply.github.com>
2023-12-20 17:11:09 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,26 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    return data
+        print(f"Random Number: {item}")
+def generate_random_data():
+    data = generate_random_data()
+
+def main():
+
+
+    main()
+import random
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+
+    return data
+    main()
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+if __name__ == "__main__":
+    for item in data:
+def generate_random_data():
+import random
